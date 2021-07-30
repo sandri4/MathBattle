81f060e36897cde728a0b68644675d0070c6aa80
sandri4 <pletin1997@outlook.com>
2021-07-30 14:10:43 +0300

    change root page, delete players route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'players#index'
-  resources :players
+  root 'battles#index'
   resources :battles
   # get 'battles#generate_task'
   post '/battles/:id', to: 'battles#show'
