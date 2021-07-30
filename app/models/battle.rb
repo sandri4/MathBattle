6b55501eb76ce7a6514b415e9211a85c90754126
sandri4 <pletin1997@outlook.com>
2021-07-30 14:09:36 +0300

    delete validates

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,5 +1,4 @@
 class Battle < ApplicationRecord
   has_many :players
   accepts_nested_attributes_for :players
-  validates :first_player_name, :second_player_name, presence: true, length: { minimum: 2 }
 end
